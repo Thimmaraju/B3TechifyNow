e0a6b94da3539414d3df74f88150f95ae5c1e859
G Thimmaraju <thimmaraju418@gmail.com>
2026-02-23 20:37:20 +0530

    objects

--- tests/PIM/login.spec.js
+++ tests/PIM/login.spec.js
@@ -1,22 +1,32 @@
 import { test, expect } from '@playwright/test';
 
 
+   let credentials = {
+
+        username : "Admin",
+        password : "admin123",
+        wrongusername: "fbfnvb"
+   }
 
   test('Verify Login with Valid Credentials', async ({ page }) => {
     await page.goto('/web/index.php/auth/login');
     console.log("Lauched URL ")
-    await page.locator("input[name='username']").fill(process.env.APP_USERNAME) //30 sec
+    await page.locator("input[name='username']").fill(credentials.username) //30 sec
     console.log("Username Entered ")
-    await page.locator("//input[@type='password']").fill(process.env.APP_PASSWORD)
+    await page.locator("//input[@type='password']").fill(credentials['password'])
     await page.getByRole('button', { name: 'Login' }).click();
     await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+
+    credentials['wrongpassword'] = "kjerngkjunwr"
+
+    delete credentials.wrongusername
   });
 
 
   test('Verify login with invalid Username and Valid Password', async ({ page }) => {
     await page.goto('/web/index.php/auth/login');
     await page.getByRole('textbox', { name: 'Username' }).click();
-    await page.getByRole('textbox', { name: 'Username' }).fill('hdvrwnugfvherw');
+    await page.getByRole('textbox', { name: 'Username' }).fill(credentials.wrongusername);
     await page.getByRole('textbox', { name: 'Password' }).click();
     await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
     await page.getByRole('button', { name: 'Login' }).click();
