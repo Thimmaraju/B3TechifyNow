1848db190830c1475afff303f48cd0bdee444b9e
G Thimmaraju <thimmaraju418@gmail.com>
2026-03-03 21:27:11 +0530

    numbers

---
+++ LearnJS/class.js
@@ -0,0 +1,62 @@
+
+// class classname{
+
+//      //Varibales
+     
+//      //Methods 
+//     //  m1
+
+//     //  m2
+
+//     //  m3
+
+//     //  m4 
+// }
+
+class calculator {
+
+    x = "Raju"
+    y= "g"
+
+    constructor(n1,n2) {
+
+        console.log(n1+n2)
+    }
+
+    Multipy(n1,n2) {
+
+            console.log(n1*n2)
+    }
+
+    Printname() {
+
+        console.log("Raju")
+    }
+
+
+}
+
+
+//Object of the classs / Instance of the class 
+
+const  cal1 = new calculator()  // Creation of Object 
+
+// cal1.add(4,8)
+
+// cal1.printName()
+
+
+// const cal2 = new calculator()
+
+// console.log(cal2.x)
+
+// cal2.add(3,8)
+
+// // constructor 
+
+// Method 
+
+// It automatically executed no call it it.
+
+
+//const cal3 = new calculator()
